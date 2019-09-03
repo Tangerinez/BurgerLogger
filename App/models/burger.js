c0bb23d0305a583eacaa35747ff64203099756d5
Tangerinez <gtang159@gmail.com>
2019-09-02 20:57:17 -0700

    Created models for BurgerLogger

--- App/models/burger.js
+++ App/models/burger.js
@@ -1,23 +1,22 @@
-var orm = require("../config/orm.js");
+var orm = require("../config/orm");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+      console.log(res);
+    });
+  },
+  insertOne: function(col, val, cb) {
+    console.log(val);
+    orm.insertOne("burgers", col, val, function(res) {
       cb(res);
     });
   },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
+  updateOne: function(input, val, cb) {
+    orm.updateOne("burgers", input, val, function(res) {
+      cb(res);
+    });
   }
 };
 
